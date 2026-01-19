d8ceba94fbf6b73b83ac0e816a4c302a6fdf0802
EdosProjects <edomanini00@gmail.com>
2026-01-19 10:48:54 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,12 @@
-self.addEventListener("install", () => {
-  console.log("Service Worker installato");
-});
 
-self.addEventListener("fetch", () => {
-  // SW minimale: nessuna cache per ora
+/* Service Worker minimale */
+self.addEventListener("install", () => {
+  // Potremo aggiungere precache in futuro
+  self.skipWaiting && self.skipWaiting();
 });
-``
+self.addEventListener("activate", (e) => {
+  e.waitUntil(self.clients.claim?.());
+});
+self.addEventListener("fetch", () => {
+  // Strategia offline semplice da aggiungere quando servono asset
+});
