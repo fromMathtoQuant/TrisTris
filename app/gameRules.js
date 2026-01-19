f0e248ff0604f36d8683655c31a386c214be976c
EdosProjects <edomanini00@gmail.com>
2026-01-19 09:40:49 +0100

    Create gameRules.js

---
+++ app/gameRules.js
@@ -0,0 +1,21 @@
+
+// Qui metteremo le regole:
+// - validazione mosse
+// - definizione della prossima microgriglia obbligata
+// - vittoria micro
+// - vittoria macro
+
+export function isValidMove(state, macroIndex, row, col) {
+  // placeholder
+  return true;
+}
+
+export function checkMicroWin(board) {
+  // placeholder
+  return null;
+}
+
+export function checkMacroWin(macroBoard) {
+  // placeholder
+  return null;
+}
