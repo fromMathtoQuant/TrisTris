b6e8ea347d6be54d312bde772e15b20dbec5d808
EdosProjects <edomanini00@gmail.com>
2026-01-19 16:16:50 +0100

    Update engine.js

--- app/engine.js
+++ app/engine.js
@@ -1,29 +1,33 @@
-  // app/engine.js
-  import { isValidMove, isMicroPlayable } from "./gameRules.js";
-  import { getMacroCellCoords } from "./boardModel.js";
-  import { checkWin } from "./gameRules.js";
-  
-  export function playMove(state, microIndex, row, col) {
-    if (!isValidMove(state, microIndex, row, col)) {
-      return false;
-    }
+// app/engine.js
+import { isValidMove, isMicroPlayable, checkWin, checkGameEnd } from "./gameRules.js";
+import { getMacroCellCoords } from "./boardModel.js";
 
+export function playMove(state, microIndex, row, col) {
+  if (!isValidMove(state, microIndex, row, col)) {
+    return false;
+  }
+  
   const symbol = state.players[state.turn];
   const board = state.microBoards[microIndex];
-
+  
   // Piazza la X / O
   board[row][col] = symbol;
-
+  
   // --- 1) Controllo vittoria micro ---
   const winner = checkWin(board);
   if (winner) {
     const { row: macroR, col: macroC } = getMacroCellCoords(microIndex);
     state.macroBoard[macroR][macroC] = winner;
   }
-
-  // --- 2) Calcolo della micro obbligata ---
+  
+  // --- 2) Controllo fine partita ---
+  const gameEnd = checkGameEnd(state);
+  if (gameEnd.finished) {
+    return { moved: true, gameEnd };
+  }
+  
+  // --- 3) Calcolo della micro obbligata ---
   const nextCandidate = row * state.microSize + col;
-
   // Se la micro candidata è giocabile → obbligata
   if (isMicroPlayable(state, nextCandidate)) {
     state.nextForcedCell = nextCandidate;
@@ -31,9 +35,9 @@
     // Altrimenti → scelta libera
     state.nextForcedCell = null;
   }
-
-  // --- 3) Cambio turno ---
+  
+  // --- 4) Cambio turno ---
   state.turn = 1 - state.turn;
-
-  return true;
+  
+  return { moved: true, gameEnd: null };
 }
